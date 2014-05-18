ce34528f09ae3ba3973e0038155c82a507de5284
viniciussanfelice@gmail.com <viniciussanfelice@gmail.com@48e7cebf-d8c2-68af-fa4c-a0d25943797a>
2014-05-18 19:23:55 +0000

    Updating Camera and Preview Create Webservice Support

--- trunk/photography/src/com/android/photography/MainActivity.java
+++ trunk/photography/src/com/android/photography/MainActivity.java
@@ -1,39 +1,58 @@
 package com.android.photography;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
 
-
 public class MainActivity extends Activity {
-	
+
 	private Button button;
 	private double latitude;
 	private double longitude;
-	
+	private Uri fileUri; // file url to store image
+	private static final String IMAGE_DIRECTORY_NAME = "Photography"; // image
+																		// directory
+																		// name
+
+	private static String photoLabel; // label takes the photo path
+	private static String photoLocation;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_layout);
-		openCamera();
+		// openCamera();
 		openGallery();
 		mapsButton();
-		
+
 		GPSTracker gps = new GPSTracker(this);
-		
-        if(gps.canGetLocation()){
-            latitude = gps.getLatitude();
-            longitude = gps.getLongitude();
-            Toast.makeText(getApplicationContext(), "Sua localização atual é: \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();    
-        }else{
-            gps.showSettingsAlert();
-        }
+
+		if (gps.canGetLocation()) {
+			latitude = gps.getLatitude();
+			longitude = gps.getLongitude();
+			Toast.makeText(
+					getApplicationContext(),
+					"Sua localização atual é: \nLat: " + latitude + "\nLong: "
+							+ longitude, Toast.LENGTH_LONG).show();
+		} else {
+			gps.showSettingsAlert();
+		}
 	}
 
 	@Override
@@ -42,25 +61,28 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
-	public void openCamera(){
-		final Context context = this;
-		button = (Button) findViewById(R.id.camerabutton);
-		button.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View view) {
-				Intent intent = new Intent(context, CameraActivity.class);
-				startActivity(intent);
-			}
-		});
+
+	// capture image and callback calls preview activity
+	public void openCamera(View v) {
+		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+		// cria a pasta e o arquivo de imagem temporario
+		fileUri = Uri.fromFile(getOutputMediaFile());
+
+		// salva a foto no local descrito
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+		if (isDeviceSupportCamera()) {
+			// start the image capture Intent
+			startActivityForResult(intent, 100);
+		}
 	}
-	
-	public void openGallery(){
+
+	public void openGallery() {
 		final Context context = this;
 		button = (Button) findViewById(R.id.gallerybutton);
 		button.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				Intent intent = new Intent(context, GalleryListActivity.class);
@@ -68,7 +90,7 @@ public class MainActivity extends Activity {
 			}
 		});
 	}
-	
+
 	public void mapsButton() {
 		final Context context = this;
 		button = (Button) findViewById(R.id.mapsbutton);
@@ -76,9 +98,78 @@ public class MainActivity extends Activity {
 
 			@Override
 			public void onClick(View v) {
-			    Intent intent = new Intent(context, MapsActivity.class);
-                startActivity(intent);   
+				Intent intent = new Intent(context, MapsActivity.class);
+				startActivity(intent);
 			}
 		});
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode == RESULT_OK) {
+			// successfully captured the image
+			// display it in image view
+			// previewCapturedImage();
+
+			Intent intent = new Intent(this, PhotoActivity.class);
+			intent.putExtra("photoLabel", this.photoLabel);
+			intent.putExtra("photoPath", fileUri.getPath());
+			// intent.putExtra("photoLocation", this.photoLocation);
+
+			startActivity(intent);
+
+			// Toast.makeText(getApplicationContext(), "Success!!!",
+			// Toast.LENGTH_SHORT).show();
+		} else if (resultCode == RESULT_CANCELED) {
+			// user cancelled Image capture
+			Toast.makeText(getApplicationContext(), "CANCELARO O BAGULHO",
+					Toast.LENGTH_SHORT).show();
+		} else {
+			// failed to capture image
+			Toast.makeText(getApplicationContext(), "NUM DEU",
+					Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	// nomeia e cria o arquivo que vai receber a foto
+	private File getOutputMediaFile() {
+
+		// External sdcard location
+		File mediaStorageDir = new File(
+				Environment
+						.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+				IMAGE_DIRECTORY_NAME);
+
+		// Create the storage directory if it does not exist
+		if (!mediaStorageDir.exists()) {
+			if (!mediaStorageDir.mkdirs()) {
+				Log.d(IMAGE_DIRECTORY_NAME, "Oops! Failed create "
+						+ IMAGE_DIRECTORY_NAME + " directory");
+				return null;
+			}
+		}
+
+		// Create a media file name
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+				Locale.getDefault()).format(new Date());
+
+		File photo = new File(mediaStorageDir.getPath() + File.separator
+				+ "IMG_" + timeStamp + ".jpg");
+
+		photoLabel = photo.getPath();
+		// photoLocation = LocationManager.getLocation(mediaFile);
+
+		return photo;
+	}
+
+	private boolean isDeviceSupportCamera() {
+		if (getApplicationContext().getPackageManager().hasSystemFeature(
+				PackageManager.FEATURE_CAMERA)) {
+			// this device has a camera
+			return true;
+		} else {
+			// no camera on this device
+			return false;
+		}
 	}
 }
