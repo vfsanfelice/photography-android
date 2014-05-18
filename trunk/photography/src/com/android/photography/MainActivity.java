0301613b2ec69c23edbbaacd6e1c7d68523da231
viniciussanfelice@gmail.com <viniciussanfelice@gmail.com@48e7cebf-d8c2-68af-fa4c-a0d25943797a>
2014-05-18 21:14:28 +0000

    Add GSON library and Camera stuff

--- trunk/photography/src/com/android/photography/MainActivity.java
+++ trunk/photography/src/com/android/photography/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends Activity {
 	private double latitude;
 	private double longitude;
 	private Uri fileUri; // file url to store image
-	private static final String IMAGE_DIRECTORY_NAME = "Photography"; // image
+	static final String IMAGE_DIRECTORY_NAME = "Photography"; // image
 																		// directory
 																		// name
 
@@ -77,6 +77,77 @@ public class MainActivity extends Activity {
 			startActivityForResult(intent, 100);
 		}
 	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode == RESULT_OK) {
+			// successfully captured the image
+			// display it in image view
+			// previewCapturedImage();
+			
+			GPSTracker gps = new GPSTracker(this);
+			latitude = gps.getLatitude();
+			longitude = gps.getLongitude();
+			
+			Intent intent = new Intent(this, PhotoActivity.class);
+			intent.putExtra("photoLabel", this.photoLabel);
+			intent.putExtra("photoPath", fileUri.getPath());
+			intent.putExtra("latitude", latitude);
+			intent.putExtra("longitude", longitude);
+			startActivity(intent);
+
+			// Toast.makeText(getApplicationContext(), "Success!!!",
+			// Toast.LENGTH_SHORT).show();
+		} else if (resultCode == RESULT_CANCELED) {
+			// user cancelled Image capture
+			Toast.makeText(getApplicationContext(), "CANCELARO O BAGULHO",
+					Toast.LENGTH_SHORT).show();
+		} else {
+			// failed to capture image
+			Toast.makeText(getApplicationContext(), "NUM DEU",
+					Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	// nomeia e cria o arquivo que vai receber a foto
+	private File getOutputMediaFile() {
+
+		// External sdcard location
+		File mediaStorageDir = new File(
+				Environment.getExternalStorageDirectory(),
+				IMAGE_DIRECTORY_NAME);
+
+		// Create the storage directory if it does not exist
+		if (!mediaStorageDir.exists()) {
+			if (!mediaStorageDir.mkdirs()) {
+				Log.d(IMAGE_DIRECTORY_NAME, "Oops! Failed create "
+						+ IMAGE_DIRECTORY_NAME + " directory");
+				return null;
+			}
+		}
+
+		// Create a media file name
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+				Locale.getDefault()).format(new Date());
+
+		File photo = new File(mediaStorageDir.getPath() + File.separator
+				+ "IMG_" + timeStamp + ".jpg");
+
+		photoLabel = photo.getPath();
+
+		return photo;
+	}
+
+	private boolean isDeviceSupportCamera() {
+		if (getApplicationContext().getPackageManager().hasSystemFeature(
+				PackageManager.FEATURE_CAMERA)) {
+			// this device has a camera
+			return true;
+		} else {
+			// no camera on this device
+			return false;
+		}
+	}
 
 	public void openGallery() {
 		final Context context = this;
@@ -102,74 +173,5 @@ public class MainActivity extends Activity {
 				startActivity(intent);
 			}
 		});
-	}
-
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (resultCode == RESULT_OK) {
-			// successfully captured the image
-			// display it in image view
-			// previewCapturedImage();
-
-			Intent intent = new Intent(this, PhotoActivity.class);
-			intent.putExtra("photoLabel", this.photoLabel);
-			intent.putExtra("photoPath", fileUri.getPath());
-			// intent.putExtra("photoLocation", this.photoLocation);
-
-			startActivity(intent);
-
-			// Toast.makeText(getApplicationContext(), "Success!!!",
-			// Toast.LENGTH_SHORT).show();
-		} else if (resultCode == RESULT_CANCELED) {
-			// user cancelled Image capture
-			Toast.makeText(getApplicationContext(), "CANCELARO O BAGULHO",
-					Toast.LENGTH_SHORT).show();
-		} else {
-			// failed to capture image
-			Toast.makeText(getApplicationContext(), "NUM DEU",
-					Toast.LENGTH_SHORT).show();
-		}
-	}
-
-	// nomeia e cria o arquivo que vai receber a foto
-	private File getOutputMediaFile() {
-
-		// External sdcard location
-		File mediaStorageDir = new File(
-				Environment
-						.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
-				IMAGE_DIRECTORY_NAME);
-
-		// Create the storage directory if it does not exist
-		if (!mediaStorageDir.exists()) {
-			if (!mediaStorageDir.mkdirs()) {
-				Log.d(IMAGE_DIRECTORY_NAME, "Oops! Failed create "
-						+ IMAGE_DIRECTORY_NAME + " directory");
-				return null;
-			}
-		}
-
-		// Create a media file name
-		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
-				Locale.getDefault()).format(new Date());
-
-		File photo = new File(mediaStorageDir.getPath() + File.separator
-				+ "IMG_" + timeStamp + ".jpg");
-
-		photoLabel = photo.getPath();
-		// photoLocation = LocationManager.getLocation(mediaFile);
-
-		return photo;
-	}
-
-	private boolean isDeviceSupportCamera() {
-		if (getApplicationContext().getPackageManager().hasSystemFeature(
-				PackageManager.FEATURE_CAMERA)) {
-			// this device has a camera
-			return true;
-		} else {
-			// no camera on this device
-			return false;
-		}
 	}
 }
