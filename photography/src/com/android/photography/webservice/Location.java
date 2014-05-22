5d10a41fc086f6a11675af213407d32df4980d2d
viniciussanfelice <viniciussanfelice@gmail.com>
2014-05-22 17:34:19 +0000

    Alter database architecture and picture_preview

--- photography/src/com/android/photography/webservice/Location.java
+++ photography/src/com/android/photography/webservice/Location.java
@@ -21,16 +21,4 @@ public class Location {
 	public void setLng(String lng) {
 		this.lng = lng;
 	}
-	
-	/*
-	{ "address" : "Itapema",
-        "cc" : "BR",
-        "city" : "Itapema",
-        "country" : "Brasil",
-        "distance" : 1176,
-        "lat" : -27.10729603036265,
-        "lng" : -48.60859473486084,
-        "postalCode" : "88220-000",
-        "state" : "SC"
-      },*/
 }
